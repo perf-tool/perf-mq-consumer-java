b66926e87f2e2b3780ef8c8a136ee55cf301ca62
Young Xu <xuthus5@gmail.com>
2022-08-22 11:27:16 +0800

    feat: kafka consumer support sasl
    fix

--- perf-mq-consumer-kafka/src/main/java/com/github/perftool/mq/consumer/kafka/AbstractKafkaPullThread.java
+++ perf-mq-consumer-kafka/src/main/java/com/github/perftool/mq/consumer/kafka/AbstractKafkaPullThread.java
@@ -22,10 +22,13 @@ package com.github.perftool.mq.consumer.kafka;
 import com.github.perftool.mq.consumer.common.AbstractPullThread;
 import com.github.perftool.mq.consumer.common.service.ActionService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.config.SaslConfigs;
+import org.apache.kafka.common.security.auth.SecurityProtocol;
 
 import java.time.Duration;
 import java.util.List;
@@ -49,6 +52,15 @@ public abstract class AbstractKafkaPullThread<T> extends AbstractPullThread {
         props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, getKeyDeserializerName());
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, getValueDeserializerName());
+        if (kafkaConfig.saslEnable) {
+            props.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, SecurityProtocol.SASL_PLAINTEXT.name);
+            props.put(SaslConfigs.SASL_MECHANISM, kafkaConfig.saslMechanism);
+            String saslJaasConfig = String.format(
+                    "org.apache.kafka.common.security.plain.PlainLoginModule required %n"
+                            + "username=\"%s\" %npassword=\"%s\";",
+                    kafkaConfig.saslUsername, kafkaConfig.saslPassword);
+            props.put(SaslConfigs.SASL_JAAS_CONFIG, saslJaasConfig);
+        }
         consumer = new KafkaConsumer<>(props);
         consumer.subscribe(topics);
     }
